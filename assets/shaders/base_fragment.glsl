164ff12aefbc4e23ebe86e1cc75b22038747bcd7
Piotrek Orlof <piotrorlof.po@gmail.com>
2026-03-28 13:34:35 +0100

    Updated Shaders

--- assets/shaders/base_fragment.glsl
+++ assets/shaders/base_fragment.glsl
@@ -1,39 +1,44 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 LightPosition;
 in vec3 FragPosition;
 in vec3 FragNormal;
 in vec2 TexCoord;
 
-// Lighting
-uniform float ambientLightStrength;
-uniform vec3 ambientLightColor;
-uniform vec3 lightStrength;
-uniform vec3 lightColor;
-uniform float specularStrength = 1.0f;
+uniform struct Light {
+    vec3 position;
+    vec3 ambientColor;
+    vec3 diffuseColor;
+    vec3 specularColor;
+} light;
+
+uniform struct Material {
+    vec3 ambientColor;
+    vec3 diffuseColor;
+    vec3 specularColor;
+    float roughness;
+} material;
 
 uniform sampler2D albedoTexture;
 
-
 void main()
 {
-    vec3 ambientLight = ambientLightColor * ambientLightStrength;
+    vec3 ambientColor = material.ambientColor * light.ambientColor;
 
     // Diffuse Lighting
     vec3 normal = normalize(FragNormal);
-    vec3 lightDirection = normalize(LightPosition - FragPosition);
+    vec3 lightDirection = normalize(light.position - FragPosition);
     float diff = max(dot(normal, lightDirection), 0.0);
-    vec3 diffuseLight = diff * lightColor;
+    vec3 diffuseColor = diff * material.diffuseColor * light.diffuseColor;
 
     // Specular Lighting
     vec3 viewDirection = normalize(FragPosition);
     vec3 reflectionDirection = reflect(-lightDirection, normal);
-    float specularIntensity = pow(max(dot(viewDirection, reflectionDirection), 0.0), 32);
+    float specularIntensity = pow(max(dot(viewDirection, reflectionDirection), 0.0), 64);
     specularIntensity *= step(0.0, dot(normal, lightDirection));
-    vec3 specularLight = specularStrength * specularIntensity * lightColor;
+    vec3 specularColor = (1.f - material.roughness) * specularIntensity * material.specularColor * light.specularColor;
 
     vec4 textureColor = texture(albedoTexture, TexCoord);
-    vec3 finalColor = textureColor.rgb * (ambientLight + diffuseLight + specularLight);
+    vec3 finalColor = textureColor.rgb * (ambientColor + diffuseColor + specularColor);
     FragColor = vec4(finalColor, textureColor.a);
 }
--- assets/shaders/base_vertex.glsl
+++ assets/shaders/base_vertex.glsl
@@ -3,22 +3,21 @@ layout (location = 0) in vec3 position;
 layout (location = 1) in vec3 normal;
 layout (location = 2) in vec2 uv;
 
-out vec3 LightPosition;
 out vec3 FragPosition;
 out vec3 FragNormal;
 out vec2 TexCoord;
 
-uniform vec3 lightPosition;
-uniform mat4 normalMatrix;
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
+uniform struct PVM{
+    mat4 model;
+    mat4 view;
+    mat4 projection;
+    mat4 normal;
+} pvm;
 
 void main(){
-    LightPosition = vec3(view * vec4(lightPosition, 1.0)); // Light Position in View Space!
-    FragPosition = (view * model * vec4(position, 1.0)).xyz;
-    FragNormal = mat3(normalMatrix) * normal;
+    FragPosition = (pvm.view * pvm.model * vec4(position, 1.0)).xyz;
+    FragNormal = mat3(pvm.normal) * normal;
     TexCoord = uv;
 
-    gl_Position = projection * view * model * vec4(position, 1.0);
+    gl_Position = pvm.projection * vec4(FragPosition, 1.0);
 }
